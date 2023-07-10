191a38fd71914064b612adbeb6eb08b717b619be
13dagger <13dagger@gmail.com>
2023-07-10 22:29:38 +0200

    feat: doctrine

--- config/bundles.php
+++ config/bundles.php
@@ -1,7 +1,13 @@
 <?php
 
 use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
+use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
+use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
+use Symfony\Bundle\MakerBundle\MakerBundle;
 
 return [
     FrameworkBundle::class => ['all' => true],
+    DoctrineBundle::class => ['all' => true],
+    DoctrineMigrationsBundle::class => ['all' => true],
+    MakerBundle::class => ['dev' => true],
 ];
